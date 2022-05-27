6069762b0afe01569067ca7ee5888fc20dbacc44
zhengjun <565061763@qq.com>
2022-05-28 02:31:53 +0800

    demo

--- app/src/main/java/com/sd/lib/demo/item_holder/item/ChildItem.kt
+++ app/src/main/java/com/sd/lib/demo/item_holder/item/ChildItem.kt
@@ -3,8 +3,12 @@ package com.sd.lib.demo.item_holder.item
 import android.util.Log
 import com.sd.lib.demo.item_holder.MainActivity
 
-class ChildItem : IParent {
+class ChildItem : IParent, AutoCloseable {
     override fun startRun() {
         Log.i(MainActivity.TAG, "startRun item:$this")
+    }
+
+    override fun close() {
+        Log.i(MainActivity.TAG, "close item:$this")
     }
 }
