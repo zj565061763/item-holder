7ff79377dd2e76564f9c9c8deec557fccc92f38b
zhengjun <565061763@qq.com>
2022-06-02 02:58:08 +0800

    holder

--- lib/src/main/java/com/sd/lib/itemholder/FItemHolder.kt
+++ lib/src/main/java/com/sd/lib/itemholder/FItemHolder.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Context
 import com.sd.lib.itemholder.impl.FActivityItemHolder
 import java.lang.reflect.Modifier
-import java.util.*
 
 open class FItemHolder<T>(target: T) {
     private val _target: T
@@ -93,6 +92,19 @@ open class FItemHolder<T>(target: T) {
     }
 
     /**
+     * 将当前对象添加到[MAP_HOLDER]
+     */
+    @Synchronized
+    fun attach(): Boolean {
+        if (isAttached) return true
+        if (onAttach()) {
+            attachHolder(this)
+            isAttached = true
+        }
+        return isAttached
+    }
+
+    /**
      * 将当前对象，从[MAP_HOLDER]移除，会清空所有Item。
      * 子类需要在合适的时机调用销毁，否则当前对象会一直被持有。
      */
@@ -109,7 +121,7 @@ open class FItemHolder<T>(target: T) {
          * 销毁逻辑真正执行之前触发销毁回调，允许子类在回调中获取Item，做一些额外的销毁操作。
          */
         onDetach()
-        remove(_target)
+        detachHolder(this)
 
         _mapItemHolder.values.forEach {
             if (it is AutoCloseable) {
@@ -124,9 +136,17 @@ open class FItemHolder<T>(target: T) {
     }
 
     /**
+     * 当前对象即将被添加到[MAP_HOLDER]，返回true-继续添加，返回false-停止添加
+     */
+    protected open fun onAttach(): Boolean {
+        return true
+    }
+
+    /**
      * 当前对象被移除回调
      */
     protected open fun onDetach() {
+
     }
 
     interface Item<T> : AutoCloseable {
@@ -137,17 +157,30 @@ open class FItemHolder<T>(target: T) {
     }
 
     companion object {
-        private val MAP_HOLDER = WeakHashMap<Any, FItemHolder<*>>()
+        private val MAP_HOLDER = mutableMapOf<Any, FItemHolder<*>>()
 
         @JvmStatic
         @Synchronized
-        fun activity(target: Context): FItemHolder<Activity> {
-            require(target is Activity) { "target must be instance of ${Activity::class.java}" }
-            val cache = MAP_HOLDER[target]
+        fun activity(context: Context): FItemHolder<Activity> {
+            require(context is Activity) { "target must be instance of ${Activity::class.java}" }
+            val cache = MAP_HOLDER[context]
             if (cache != null) return cache as FActivityItemHolder
 
-            val holder = FActivityItemHolder(target)
-            if (!target.isFinishing) {
+            val holder = FActivityItemHolder(context)
+            if (holder.attach()) {
+                MAP_HOLDER[context] = holder
+            }
+            return holder
+        }
+
+        @JvmStatic
+        @Synchronized
+        fun <T : Any> target(target: T): FItemHolder<T> {
+            val cache = MAP_HOLDER[target]
+            if (cache != null) return cache as FItemHolder<T>
+
+            val holder = FItemHolder(target)
+            if (holder.attach()) {
                 MAP_HOLDER[target] = holder
             }
             return holder
@@ -155,10 +188,19 @@ open class FItemHolder<T>(target: T) {
 
         @JvmStatic
         @Synchronized
-        private fun remove(target: Any?) {
-            if (target != null) {
-                MAP_HOLDER.remove(target)
+        private fun attachHolder(holder: FItemHolder<*>) {
+            val target = holder._target!!
+            if (MAP_HOLDER.containsKey(target)) {
+                throw RuntimeException("target holder has been attached.")
+            } else {
+                MAP_HOLDER[target] = holder
             }
+        }
+
+        @JvmStatic
+        @Synchronized
+        private fun detachHolder(holder: FItemHolder<*>) {
+            MAP_HOLDER.remove(holder._target)
         }
     }
 }
