3a20bf7a88d2b92264971c9872d93b5d516a4f8c
zhengjun <565061763@qq.com>
2022-06-02 04:05:25 +0800

    demo

--- app/src/main/java/com/sd/lib/demo/item_holder/MainActivity.kt
+++ app/src/main/java/com/sd/lib/demo/item_holder/MainActivity.kt
@@ -40,6 +40,14 @@ class MainActivity : AppCompatActivity() {
         item?.startRun()
     }
 
+    fun testCrash() {
+        val holder1 = FItemHolder.target(TAG)
+        val holder2 = FItemHolder.target(TAG)
+        require(holder1 !== holder2)
+        holder1.attach()
+        holder2.attach()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         Handler(Looper.getMainLooper()).postDelayed({
